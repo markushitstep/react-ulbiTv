2ceb89111783e03d8ba24fc60cb8050cc34ed05b
Max <markushitstep@gmail.com>
2022-04-14 10:38:15 +0300

    add private and public routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import About from '../pages/About'
-import Posts from '../pages/Posts'
-import Error from '../pages/Error'
-import PostIdPage from '../pages/PostIdPage'
+import { privateRoutes, publicRoutes } from '../router'
 
 const AppRouter = () => {
+    const isAuth = false;
     return (
-        <Routes>
-            <Route path='/about' element={<About />} />
-            <Route exact path='/posts' element={<Posts />} />
-            <Route exact path='/posts/:id' element={<PostIdPage />} />
-            <Route path="*"  element={<Posts />} />
-        </Routes>
+        isAuth
+            ?   <Routes>
+                    {privateRoutes.map((route) => 
+                        <Route 
+                            path={route.path} 
+                            element={route.component} 
+                            exact={route.exact}
+                        />
+                    )}
+                </Routes>
+            :   <Routes>
+                    {publicRoutes.map((route) => 
+                        <Route 
+                            path={route.path} 
+                            element={route.component} 
+                            exact={route.exact}
+                        />
+                    )}
+                </Routes>
+        
     )
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MyButton from '../components/UI/button/MyButton';
+import MyInput from '../components/UI/input/MyInput';
+
+const Login = () =>{
+
+    return (
+        <div>
+           <h1>Страница для логина</h1>
+           <form>
+                <MyInput type="text" placeholder='введите логин'/>
+                <MyInput type="password" placeholder='введите пороль'/>
+                <MyButton>Войти</MyButton>
+           </form>
+        </div>
+    )
+}
+
+export default Login;
